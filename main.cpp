3a933b15fea346004b5beca3a29814f646c47795
Ujjwal Yadav <147945613+cotaxe@users.noreply.github.com>
2024-11-06 21:30:32 +0530

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,3 @@
-/*
-Jack Martin
-
-ChatBot
-Version 1.0
-
-This program creates a simple chatbot
-
-Allows user to ask bot a question
-If the bot knows the answer to the question, the bot will reply with the answer
-If the bot does not know the answer to the question, the bot will ask the user the same question and store the answer for future use
-*/
-
-
 #include "bot.h"
 #include "question.h"
 
